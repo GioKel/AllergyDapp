ec3d55526647219f84394dd1d44e8e7eaa703bc0
GioKel <gkldts@gmail.com>
2021-04-28 13:05:58 +0300

    Updates: home page add product page view search page view
    loading button
    modal on addition result

--- AllergyDapp/src/allergydapp.js
+++ AllergyDapp/src/allergydapp.js
@@ -23,6 +23,7 @@ const accountsDiv = document.getElementById('accounts')
 // Basic Actions Section
 const onboardButton = document.getElementById('connectButton');
 const submitButton = document.getElementById('submitButton');
+const searchButton = document.getElementById('searchButton');
 const getAccountsButton = document.getElementById('getAccounts')
 const getAccountsResults = document.getElementById('getAccountsResult')
 
@@ -35,8 +36,23 @@ const wheat = document.getElementById('wheat');
 const soya = document.getElementById('soya');
 const nuts = document.getElementById('nuts');
 const shellfish = document.getElementById('shellfish');
+const sProductID = document.getElementById('sProductID');
 
-  
+//Tabs
+const addProductIgredientsTab = document.getElementById('addProductIgredients');
+const searchProductIgredientsTab = document.getElementById('searchProductIgredients');
+
+//Tab links
+const addLink = document.getElementById('addLink');
+const searchLink = document.getElementById('searchLink');
+
+//Table
+const searchResultContainer = document.getElementById('searchResult');
+const spinner = document.getElementById('spinner');
+const loadingText = document.getElementById('loadingText');
+const submitText = document.getElementById('submitText');
+const addProductModalText = document.getElementById('addProductModalText');
+
 const initialize = async () => {
 	  let onboarding
 	  try {
@@ -85,6 +101,7 @@ const initialize = async () => {
 			  onboardButton.innerText = 'Connected'
 			  onboardButton.disabled = true
 			  submitButton.disabled = false
+			  searchButton.disabled = false
 			  if (onboarding) {
 			    onboarding.stopOnboarding()
 			  }
@@ -102,6 +119,13 @@ const initialize = async () => {
 		
 		  
 		  submitButton.onclick = async () => {
+			  
+			  
+        	  spinner.className = spinner.className.replace("visually-hidden", "");
+			  loadingText.className = loadingText.className.replace("visually-hidden", "");
+			  submitText.setAttribute("class", submitText.getAttribute("class")+ " visually-hidden");
+			  submitButton.disabled = true
+			  
 			  let inputs = {
 				  "productName": productName.value,
 				  "productID": productID.value,
@@ -111,6 +135,69 @@ const initialize = async () => {
 				  "soya": soya.value,
 				  "nuts": nuts.value,
 				  "shellfish": shellfish.value
+			  }
+			  
+			  console.log(inputs)	
+			  
+		        try {
+		          web3.eth.defaultAccount = ethereum.selectedAddress;
+		         
+		          var resultModal = new bootstrap.Modal(document.getElementById('resultModal'), {
+		        	  keyboard: false
+		        	})
+		          
+		          var abi = [{"constant":false,"inputs":[{"name":"_productName","type":"string"},{"name":"_productID","type":"uint256"},{"name":"_milk","type":"uint256"},{"name":"_egg","type":"uint256"},{"name":"_wheat","type":"uint256"},{"name":"_soya","type":"uint256"},{"name":"_nuts","type":"uint256"},{"name":"_shellfish","type":"uint256"}],"name":"addProductIngredients","outputs":[{"name":"","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"_searchProductbyID","type":"uint256"}],"name":"searchProductIngredients","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"size","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"dAppPurpose","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+				  var allergydContract = new web3.eth.Contract(abi,
+						  '0xa92815Fcf37A1f7Fd6a0AB6d8d97352Bc82611Ad',
+						  {
+						    from: ethereum.selectedAddress // default from address
+						});
+	        	  allergydContract.defaultChain= "rinkeby";
+	        	  console.log(allergydContract);
+	        	    
+				
+	        	  
+	        	  console.log('Purpose');
+	        	  const purposedApp = await allergydContract.methods.dAppPurpose(
+					).call()
+					.then(function(result){
+			        	  console.log(result);
+					});
+	        	  
+	        	  console.log('addProductIngredients');
+	        	  const addProductIngredients = await allergydContract.methods.addProductIngredients(
+							productName.value,
+							productID.value,
+							milk.value,
+							egg.value,
+							wheat.value,
+							soya.value,
+							nuts.value,
+							shellfish.value
+					).send({from: ethereum.selectedAddress})
+					.then(function(result){
+			        	  console.log(result);
+			        	  addProductModalText.innerHTML = "Το προιόν προστέθηκε!" 
+			        	  resultModal.show()
+					});
+	        	  
+						
+		        } catch (error) {
+		        	  console.error(error);
+		        	  addProductModalText.innerHTML = "Αποτυχία προσθήκης!" 
+		        	  resultModal.show()
+		        }
+		        
+				submitText.className = submitText.className.replace("visually-hidden", "");
+				spinner.setAttribute("class", spinner.getAttribute("class")+ " visually-hidden");
+				loadingText.setAttribute("class", loadingText.getAttribute("class")+ " visually-hidden");
+				submitButton.disabled = false
+			  ///////////////////////////////////////////////////////////////
+		  }
+		  
+		  searchButton.onclick = async () => {
+			  let inputs = {
+				  "productID": sProductID.value,
 			  }
 			  
 			  console.log(inputs)	
@@ -129,32 +216,29 @@ const initialize = async () => {
 	        	    
 				
 	        	  
+	        	  console.log('Purpose');
 	        	  const purposedApp = await allergydContract.methods.dAppPurpose(
 					).call()
 					.then(function(result){
 			        	  console.log(result);
 					});
 	        	  
-	        	  const allergydapp = await allergydContract.methods.addProductIngredients(
-							productName.value,
-							productID.value,
-							milk.value,
-							egg.value,
-							wheat.value,
-							soya.value,
-							nuts.value,
-							shellfish.value
-					).send({from: ethereum.selectedAddress})
+	        	  
+	        	  console.log('searchProductIngredients: '+ sProductID.value);
+	        	  const searchProductIngredients = await allergydContract.methods.searchProductIngredients(
+	        			  sProductID.value,
+					).call({from: ethereum.selectedAddress})
 					.then(function(result){
 			        	  console.log(result);
+			        	  
+			        	  searchResultContainer.className = searchResultContainer.className.replace("invisible", "visible");
+			        	  createTableResults (result)
+			        	  
 					});
 						
 		        } catch (error) {
 		        	  console.error(error);
 		        }
-			  
-			
-		        
 			  
 			  
 			  ///////////////////////////////////////////////////////////////
@@ -182,8 +266,67 @@ const initialize = async () => {
 		}
 		updateButtons()
 	}
+	
+	function createTableResults (result) {
+		  var tBody = document.getElementById("searchResultBody");
+		  var child = document.getElementById("searchResultBody").childNodes[0];
+	  	  console.log(tBody);
+	  	  var tr = document.createElement("tr"); 
+	  	  
+	  	  var num = document.createElement("th");                       
+	  	  var numVal = document.createTextNode("1"); 
+	  	  num.setAttribute("scope","row");
+	  	  num.appendChild(numVal);                                          
+	  	  
+	  	  var name = document.createElement("td");                       
+	  	  var nameVal = document.createTextNode(result[0]); 
+	  	  name.appendChild(nameVal);                                          
+	
+	  	  var milk = document.createElement("td");                       
+	  	  var milkVal = document.createTextNode(result[1]); 
+	  	  milk.appendChild(milkVal);                                          
+	  	  
+	  	  var egg = document.createElement("td");                       
+	  	  var eggkVal = document.createTextNode(result[2]); 
+	  	  egg.appendChild(eggkVal);  
+	  	  
+	  	  var wheat = document.createElement("td");                       
+	  	  var wheatkVal = document.createTextNode(result[3]); 
+	  	  wheat.appendChild(wheatkVal);  
+	  	  
+	  	  var soya = document.createElement("td");                       
+	  	  var soyaVal = document.createTextNode(result[4]); 
+	  	  soya.appendChild(soyaVal);  
+	  	  
+	  	  var nuts = document.createElement("td");                       
+	  	  var nutsVal = document.createTextNode(result[5]); 
+	  	  nuts.appendChild(nutsVal);  
+	  	  
+	  	  var shellfish = document.createElement("td");                       
+	  	  var shellfishVal = document.createTextNode(result[6]); 
+	  	  shellfish.appendChild(shellfishVal);  
+	  	  
+	  	  tr.appendChild(num);
+	  	  tr.appendChild(name);
+	  	  tr.appendChild(milk);
+	  	  tr.appendChild(egg);
+	  	  tr.appendChild(wheat);
+	  	  tr.appendChild(soya);
+	  	  tr.appendChild(nuts);
+	  	  tr.appendChild(shellfish);
+	  	  
+	  	  if(tBody.hasChildNodes()){
+	  		  tBody.replaceChild(tr, tBody.childNodes[0]);
+	  	  }else{
+		  	  tBody.appendChild(tr);
+	  	  }
+	  	  
 
-	  updateButtons()	
+	}
+	
+
+
+	updateButtons()	
 	  
 	  if (isMetaMaskInstalled()) {
 
@@ -202,6 +345,7 @@ const initialize = async () => {
 	  }
   
 };
+
 	
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
@@ -214,4 +358,6 @@ if (typeof web3 !== 'undefined') {
 }
 
 window.addEventListener('DOMContentLoaded', initialize)
+
+
 
